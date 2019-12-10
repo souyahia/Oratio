a0828ff85310224c5782ebaef745216bc39456d8
souyahia <ouyahia.Samy@yahoo.fr>
2019-12-10 08:13:32 +0100

    Removed Google API

--- oratio-checklist/src/environments/environment.ts
+++ oratio-checklist/src/environments/environment.ts
@@ -2,18 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { firebase } from '../../../apikeys/firebaseApiKey';
+
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBaE1Bp1-9gOPTakq_oNGzysTNpTuMP3cY",
-    authDomain: "projet-s9-oratio.firebaseapp.com",
-    databaseURL: "https://projet-s9-oratio.firebaseio.com",
-    projectId: "projet-s9-oratio",
-    storageBucket: "projet-s9-oratio.appspot.com",
-    messagingSenderId: "926743468005",
-    appId: "1:926743468005:web:55b3bc49557b770355410c",
-    measurementId: "G-4727P8X2HZ"
-  }
+  firebase: firebase
 };
 
 /*
